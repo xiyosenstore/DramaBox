7e86b79f42278fd12639314d6ad22cdf368ee5a5
zerokit-769 <haidliralimutan@gmail.com>
2026-01-06 05:14:08 +0700

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,6 @@ export const config = {
   PROJECT_NAME: process.env.PROJECT_NAME || "DramaBoxGratis",
   BUSSINESS_CONTACT_TELE:
     process.env.NEXT_PUBLIC_BUSSINESS_CONTACT_TELE ||
-    "https://t.me/dramaboxgratis",
+    "https://t.me/Bleszh",
   NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID || "G-XXXXXXX",
 };
